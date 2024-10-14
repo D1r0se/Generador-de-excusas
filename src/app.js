48ca85edac03ac39e04ee530f559b571faaf3b62
D1r0se <154914732+D1r0se@users.noreply.github.com>
2024-10-14 19:34:44 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let excusa =
+    who[parseInt(Math.random() * 4)] +
+    " " +
+    action[parseInt(Math.random() * 4)] +
+    " " +
+    what[parseInt(Math.random() * 3)] +
+    " " +
+    when[parseInt(Math.random() * 5)];
+
+  document.getElementById("excuse").innerHTML = excusa;
 };
